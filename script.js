493e570d33abc1b06d42a99c729d986f6cc6d348
Kevin Luong <kluon030@uottawa.ca>
2024-06-04 10:45:23 -0400

    user is able to enter letters and have it check against the actual word

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var Game = (function() {
     var game = {};
-    game.score = 0.0;
-    game.highScore = 0.0;
+    game.score = 0;
+    game.highScore = 0;
     game.currentWord = "";
     game.wordInventory = ["Happy", "Plain", "Angry"]; //list of possible words to guess
     game.guesses = []; //list of all user's guesses
@@ -61,14 +61,14 @@ var Game = (function() {
             currentArray[correctPositions[i]] = null;
         }
 
+        //check if the letter is in the list. if it is, set the letter to null in current so that we don't double count.
         for (let i = 0; i < inputArray.length; i++) {
-            index = currentArray.findIndex(function(value, index, array) {
-                return value == inputArray[i];
-            });
-            
-            if (index > 0) {
-                correctLetters.push(index);
-                currentArray[index] = null;
+            for (let j = 0; j < currentArray.length; j++) {
+                console.log(inputArray[i]);
+                if (inputArray[i] == currentArray[j]) {
+                    correctLetters.push(i);
+                    currentArray[j] = null;
+                }
             }
         }
 
@@ -84,9 +84,9 @@ var Game = (function() {
         else if (!isAlpha(input)) {
             return "Input can only contain letters";
         }
-        else if (!isWord(input)) {
-            return "Not a word";
-        }
+        // else if (!isWord(input)) {
+        //     return "Not a word";
+        // }
         //add guess to list
         game.guesses.push(input);
         
@@ -105,8 +105,73 @@ var Game = (function() {
         correctPositions = checkPositions(inputArray, current);
         correctLetters = checkLetters(inputArray, current, correctPositions)
 
-        return {"positions": correctPositions, "letters": correctLetters};
+        return [correctPositions, correctLetters];
     }
 
     return game;
+});
+
+var games = []; //stores a list of all games played
+var game = Game();
+game.startGame();
+
+var guess = [];
+var attempts = 0; //the row/word that the user is currently on
+var letter = 0; //the letter that the user is entering
+var tiles = document.getElementsByClassName("tile");
+var inputLetter = document.getElementsByClassName("input-letter");
+
+window.addEventListener("keydown", (event) => {
+    //for a single character that is pressed down, check that it is a letter (lowercase or uppercase)
+    const pattern = /[a-zA-Z]/;
+    if (event.key.length == 1 && pattern.test(event.key)) {
+        if (guess.length < 5 && attempts < 6) {
+            inputLetter[(attempts * 5) + letter].innerHTML = event.key.toUpperCase();
+            guess.push(event.key.toUpperCase());
+            letter++;
+        }
+    }
+    //if the key pressed was backspace, erase a letter
+    else if (event.key == "Backspace") {
+        if (guess.length > 0) {
+            letter--;
+            inputLetter[(attempts * 5) + letter].innerHTML = "";
+            guess.pop();
+        }
+
+        console.log(guess);
+        console.log("'Backspace' was pressed.");
+    }
+    //if the key pressed was enter and the user entered 5 letters, submit the guess
+    else if (event.key == "Enter" && guess.length == 5) {
+        console.log("'Enter' was pressed.");
+        let result = game.checkWord(guess.join(""));
+        console.log(result);
+
+        //guessed word is correct (game ends)
+        if (result == true) {
+            for (let i = 0; i < 5; i++) {
+                tiles[(attempts * 5) + i].style.backgroundColor = "green";
+            }
+        }
+        //a list of correct positions and letters was returned
+        if (result.length == 2) {
+            let correctPositions = result[0];
+            let correctLetters = result[1];
+
+            for (let i = 0; i < correctPositions.length; i++) {
+                tiles[(attempts * 5) + correctPositions[i]].style.backgroundColor = "green";
+            }
+
+            for (let i = 0; i < correctLetters.length; i++) {
+                tiles[(attempts * 5) + correctLetters[i]].style.backgroundColor = "yellow";
+            }
+        }
+
+        if (attempts < 6) {
+            attempts++;
+        }
+        letter = 0;
+        guess = [];
+    }
 });
