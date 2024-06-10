d022a294539f325b6c70583ac6838f8b6678f240
Jessica Chen <Jessica10chen@gmail.com>
2024-06-10 01:31:33 -0400

    Add title and how to play button

--- script.js
+++ script.js
@@ -284,3 +284,13 @@ function startGame() {
 }
 
 window.addEventListener("load", startGame);
+
+// When user clicks on button, show message
+function show(message){
+    document.getElementById(message).style.display = 'block'
+}
+
+// When user clicks on button, hide mssage
+function hide(message){
+    document.getElementById(message).style.display = 'none'
+}
