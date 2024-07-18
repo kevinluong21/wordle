69d4ec1e214175614118ff93855b0054bb376c4c
Jessica Chen <Jessica10chen@gmail.com>
2024-07-17 20:17:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -124,6 +124,7 @@ function submitGuess() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             try {
+                console.log(this.responseText);
                 var response = JSON.parse(this.responseText);
                 var games = response["games"];
                 var guess = response["guess"];
